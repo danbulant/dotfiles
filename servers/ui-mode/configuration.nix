fe3e1aa385db109f28f00350ed9153d9f1d60e97
Daniel Bulant <danbulant@gmail.com>
2026-04-23 21:24:20 +0200

    update

--- servers/ui-mode/configuration.nix
+++ servers/ui-mode/configuration.nix
@@ -195,6 +195,7 @@ in
       "https://install.determinate.systems"
       "https://cache.garnix.io"
       "https://rusic.cachix.org"
+      "https://cuda-maintainers.cachix.org"
     ];
     trusted-public-keys = [
       "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
@@ -204,6 +205,7 @@ in
       "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
       "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
       "rusic.cachix.org-1:WXMpGpamblLUiJtcoxBxGGGGwIcWxGPJBUxarLiqWmw="
+      "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="
     ];
   };
 
