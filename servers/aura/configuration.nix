9407985eb32a24aedad2b1de2c8e183c654ffa46
Daniel Bulant <danbulant@gmail.com>
2026-05-30 11:55:48 +0200

    aura cfg

---
+++ servers/aura/configuration.nix
@@ -0,0 +1,16 @@
+{ pkgs, ... }:
+
+{
+  hardware.graphics.extraPackages = with pkgs; [
+    # Required for modern Intel GPUs (Xe iGPU and ARC)
+    intel-media-driver # VA-API (iHD) userspace
+    vpl-gpu-rt # oneVPL (QSV) runtime
+
+    # Optional compute/tooling for Intel GPUs
+    intel-compute-runtime # OpenCL (NEO) + Level Zero for Arc/Xe
+  ];
+
+  environment.sessionVariables = {
+    LIBVA_DRIVER_NAME = "iHD";
+  };
+}
